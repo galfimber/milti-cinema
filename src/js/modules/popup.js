e405cf43443d9b1bb8fd3acef6b7c48353df483e
galfimber <galfimber2014@yandex.ru>
2023-10-07 20:11:00 +0500

    correct popup v2

--- src/js/modules/popup.js
+++ src/js/modules/popup.js
@@ -20,7 +20,7 @@ export default function FilmInfo(film) {
   allActros = allActros.slice(0, -2);
 
   let trailer = "";
-  if (film.videos) {
+  if (film.videos && film.videos.trailers.length > 0) {
     //trailer = film.videos.trailers[0].url;
     trailer = `<iframe allowfullscreen class="film__trailer"src="${film.videos.trailers[0].url}"></iframe>`;
   } else {
@@ -47,6 +47,7 @@ export default function FilmInfo(film) {
     <div class="popup__content">
       ${trailer}
       <div class="film__info">
+        <div class="film__watch"></div>
         <div class="film__genre">${allGenres}</div>
         <div class="film__countries">${allCountries}</div>
         <div class="film__year">${film.year}</div>
@@ -56,6 +57,24 @@ export default function FilmInfo(film) {
       </div>
     </div>
   </div>`;
+
+  const link = [];
+  const linkLogo = [];
+  if (film.watchability && film.watchability.items.length > 0) {
+    for (let i = 0; i < film.watchability.items.length; i++) {
+      linkLogo[i] = film.watchability.items[i].logo.url;
+      link[i] = film.watchability.items[i].url;
+    }
+    const filmWatch = document.querySelector(".film__watch");
+    filmWatch.innerHTML = `
+    <h3 class="title-3">Можно посмотреть</h2>
+    <ul class="film__cinema"></ul>`;
+    const cinemaList = document.querySelector(".film__cinema");
+    cinemaList.innerHTML = "";
+    for (let i = 0; i < link.length; i++) {
+      cinemaList.innerHTML += `<li class="film__cinema--item"><a class="film__link" href="${link[i]}" target="_blank"><img class="film__link--img" src="${linkLogo[i]}"></a></li>`;
+    }
+  }
 
   //const popupClose = document.querySelector(".popup__close");
   //popupClose.addEventListener("click", (e) => {
@@ -74,6 +93,8 @@ export default function FilmInfo(film) {
       e.preventDefault();
       popup.classList.remove("popup--open");
       document.body.classList.remove("no-scroll"); // то закрываем окно навигации, удаляя активный класс
+      popup.innerHTML = "";
+      cinemaList.innerHTML = "";
     }
   });
 
